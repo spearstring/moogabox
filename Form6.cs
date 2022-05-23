434ceb8b8ce9d602ba8b83a5a9f73e940a47cf58
Jitae9605 <dlwlxo3819@naver.com>
2022-05-23 12:07:00 +0900

    모든 cs에 using System.Data.sqlclient추가

--- Form6.cs
+++ Form6.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 using WinFormsApp1;
 
 namespace moogabox
@@ -49,7 +50,8 @@ namespace moogabox
 
 		private void DataLoad()
 		{
-
+			var Conn = new SqlConnection(Constr);
+			Conn.Open();
 		}
 
 		private void btnCancel_Click(object sender, EventArgs e)
